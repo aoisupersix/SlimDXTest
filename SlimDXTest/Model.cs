55b7b7432799bdedc5b17d17b8a6bde45296f4d2
aoisupersix <r58460@st.takushoku-u.ac.jp>
2017-10-23 17:53:42 +0900

    リファクタリング
    3DModelの処理をModelクラスに移動/不要なファイルを削除

---
+++ SlimDXTest/Model.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using SlimDX;
+using Dx11 = SlimDX.Direct3D11;
+using Dxgi = SlimDX.DXGI;
+using D3DComp = SlimDX.D3DCompiler;
+
+namespace SlimDXTest
+{
+    /// <summary>
+    /// Xファイルからのモデルをまとめて管理する
+    /// </summary>
+    class Model
+    {
+        const int VTXNUM_SQUARE = 4; //頂点の数が4つの頂点インデックス
+
+        public string FilePath { get; set; }
+        public string OldPath { get; }
+        XFileConverter Cnv;
+
+        Dx11.InputLayout vertexLayout;
+        Dx11.Buffer vertexBuffer;
+        Dx11.Buffer indexBuffer;
+        List<VertexPositionTexture> vtxList = new List<VertexPositionTexture>();
+        List<uint> indices = new List<uint>();
+        TextureManager textureManager = new TextureManager();
+
+        public Model(string filePath)
+        {
+            this.OldPath = filePath;
+            Cnv = new XFileConverter(this.OldPath, CreateNewPath(filePath));
+            this.FilePath = Cnv.Import();
+        }
+
+        /// <summary>
+        /// 変換後のXファイルのパスを生成する。
+        /// とりあえず暫定として1階層上のディレクトリをtmpフォルダに入れる
+        /// </summary>
+        /// <param name="filePath">xファイルのパス</param>
+        /// <returns>変換後のファイルのパス</returns>
+        private string CreateNewPath(string filePath)
+        {
+            string currentDirectoryPath = System.IO.Path.GetFullPath("./");
+            string tmpPath = currentDirectoryPath + @"temp\";
+
+            string[] splitPath = System.IO.Path.GetFullPath(filePath).Split('\\');
+            string origin = splitPath[splitPath.Length - 2] + @"\" + splitPath[splitPath.Length - 1];
+
+            return tmpPath + origin;
+        }
+
+        /// <summary>
+        /// マテリアル名から一致するマテリアルを調べる
+        /// </summary>
+        /// <param name="materialReference">参照マテリアル名</param>
+        /// <returns>参照先マテリアル</returns>
+        private Material GetMaterialFromReference(string materialReference)
+        {
+            foreach (Material m in Cnv.matList.materials)
+            {
+                if (m.Name.Equals(materialReference))
+                    return m;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// このモデルのバッファをデバイスにセットする
+        /// </summary>
+        public void InitModelInputAssembler(Dx11.Device device)        {
+
+            device.ImmediateContext.InputAssembler.InputLayout = vertexLayout;
+            device.ImmediateContext.InputAssembler.SetVertexBuffers(
+                0,
+                new Dx11.VertexBufferBinding(vertexBuffer, VertexPositionTexture.SizeInBytes, 0)
+                );
+
+            device.ImmediateContext.InputAssembler.SetIndexBuffer(indexBuffer, Dxgi.Format.R32_UInt, 0);
+
+            device.ImmediateContext.InputAssembler.PrimitiveTopology
+                = Dx11.PrimitiveTopology.TriangleList;
+        }
+
+        /// <summary>
+        /// モデルを描画する
+        /// </summary>
+        public void DrawModel(Dx11.Device device,Dx11.Effect effect)
+        {
+            int startIdx = 0, indexCount = 0;
+            for (int i = 0; i < Cnv.meshSection.matList.PolygonCount; i++)
+            {
+                //マテリアル名から一致するマテリアルを調べる
+                int matNum = Cnv.meshSection.matList.materialIndex[i];
+                string refName = Cnv.meshSection.matList.materialReferens[matNum];
+
+                Material m = GetMaterialFromReference(refName);
+                Console.WriteLine("mat:" + matNum + "-> ref:" + refName + ",matName:" + m.Name);
+
+                //テクスチャを利用しているかどうか
+                if (!m.TextureFileName.Equals(""))
+                {
+                    effect.GetVariableByName("tex").AsScalar().Set(true);
+                    effect.GetVariableByName("normalTexture").AsResource().SetResource(textureManager.GetTexture(m.Name));
+                }
+                else
+                {
+                    effect.GetVariableByName("tex").AsScalar().Set(false);
+                    effect.GetVariableByName("mat").AsScalar().Set(true);
+                    effect.GetVariableByName("matColor").AsVector().Set(m.DiffuseColor);
+                }
+
+                //頂点数が4の場合は2つに分割する
+                if (Cnv.meshSection.meshList.mesh[i].Length == VTXNUM_SQUARE)
+                {
+                    indexCount = 6;
+                    effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply(device.ImmediateContext);
+                    device.ImmediateContext.DrawIndexed(indexCount, startIdx, 0);
+                }
+                else
+                {
+                    indexCount = 3;
+                    effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply(device.ImmediateContext);
+                    device.ImmediateContext.DrawIndexed(indexCount, startIdx, 0);
+                }
+                startIdx += indexCount;
+            }
+
+            effect.GetVariableByName("tex").AsScalar().Set(false);
+            effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply(device.ImmediateContext);
+            device.ImmediateContext.DrawIndexed(indices.Count, 0, 0);
+        }
+
+        /// <summary>
+        /// 初期化処理を行う
+        /// </summary>
+        public void Init(Dx11.Device device, Dx11.Effect effect)
+        {
+            InitVertexLayout(device, effect);
+            InitVertexBuffer(device);
+            InitIndexBuffer(device);
+            InitTexture(device);
+        }
+
+        private void InitVertexLayout(Dx11.Device device, Dx11.Effect effect)
+        {
+            vertexLayout = new Dx11.InputLayout(
+                device,
+                effect.GetTechniqueByIndex(0).GetPassByIndex(0).Description.Signature, VertexPositionTexture.VertexElements);
+        }
+
+        /// <summary>
+        /// 頂点バッファの格納
+        /// </summary>
+        /// <param name="device">GraphicsDevice</param>
+        private void InitVertexBuffer(Dx11.Device device)
+        {
+            for (int i = 0; i < Cnv.meshSection.vtxList.VertexCount; i++)
+            {
+                Vector3 vtxPos = Cnv.meshSection.vtxList.vertex[i];
+                Vector2 uvPos = Cnv.meshSection.uvList.uvs[i];
+
+                Console.WriteLine("Model:" + OldPath + " -> InitVertexBuffer");
+                Console.WriteLine("AddVertex [" + i + "] : x:" + vtxPos.X + ",y:" + vtxPos.Y + ",z:" + vtxPos.Z);
+                Console.WriteLine("AddUv [" + i + "] : x:" + uvPos.X + ",y:" + uvPos.Y);
+
+                vtxList.Add(MyDirectXHelper.CreateVertexPositionTexture(vtxPos, uvPos));
+            }
+
+            vertexBuffer = MyDirectXHelper.CreateVertexBuffer(device, vtxList.ToArray());
+        }
+
+        /// <summary>
+        /// インデックスバッファの格納
+        /// </summary>
+        /// <param name="device">GraphicsDevice</param>
+        private void InitIndexBuffer(Dx11.Device device)
+        {
+            int counter = 0;
+            foreach (int[] meshes in Cnv.meshSection.meshList.mesh)
+            {
+                Console.WriteLine("index:" + counter);
+                if (meshes.Length == VTXNUM_SQUARE)
+                {
+                    Console.WriteLine("SQUARE:1 " + meshes[0] + ", " + meshes[1] + ", " + meshes[3]);
+                    Console.WriteLine("SQUARE:2 " + meshes[1] + ", " + meshes[2] + ", " + meshes[3]);
+
+                    //四角形を2つに分割してインデックスバッファに格納
+                    indices.Add((uint)meshes[0]);
+                    indices.Add((uint)meshes[1]);
+                    indices.Add((uint)meshes[3]);
+
+                    indices.Add((uint)meshes[1]);
+                    indices.Add((uint)meshes[2]);
+                    indices.Add((uint)meshes[3]);
+                }
+                else
+                {
+                    Console.WriteLine("TRIANGLE:1 " + meshes[0] + ", " + meshes[1] + ", " + meshes[2]);
+                    //三角形なのでそのままインデックスバッファに格納
+                    indices.Add((uint)meshes[0]);
+                    indices.Add((uint)meshes[1]);
+                    indices.Add((uint)meshes[2]);
+                }
+                counter++;
+            }
+            indexBuffer = MyDirectXHelper.CreateIndexBuffer(device, indices.ToArray());
+        }
+
+        /// <summary>
+        /// テクスチャの格納
+        /// </summary>
+        /// <param name="device">GraphicsDevice</param>
+        private void InitTexture(Dx11.Device device)
+        {
+            string directoryPath = System.IO.Path.GetDirectoryName(this.FilePath) + "/";
+
+            //テクスチャの抽出
+            for (int i = 0; i < Cnv.matList.MaterialCount; i++)
+            {
+                if (!Cnv.matList.materials[i].TextureFileName.Equals(""))
+                {
+                    //テクスチャ登録
+                    try
+                    {
+                        textureManager.SetTexture(
+                            Cnv.matList.materials[i].Name,
+                            Dx11.ShaderResourceView.FromFile(
+                                device, directoryPath + Cnv.matList.materials[i].TextureFileName,
+                                new Dx11.ImageLoadInformation()
+                                {
+                                    Format = Dxgi.Format.R32G32B32A32_Float,
+                                    FilterFlags = Dx11.FilterFlags.Triangle,
+                                    MipFilterFlags = Dx11.FilterFlags.Triangle,
+                                    Usage = Dx11.ResourceUsage.Default,
+                                    BindFlags = Dx11.BindFlags.ShaderResource,
+                                    MipLevels = -1
+                                }
+                                )
+                            );
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Texture:" + Cnv.matList.materials[i].TextureFileName + " is not found.");
+                        continue;
+                    }
+                }
+            }
+        }
+
+        public void Unload()
+        {
+            foreach (var t in textureManager.GetAllTextures()) t?.Dispose();
+            vertexLayout.Dispose();
+            vertexBuffer.Dispose();
+            indexBuffer.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// テクスチャの管理クラス
+    /// </summary>
+    class TextureManager
+    {
+        private List<Dx11.ShaderResourceView> textures = new List<Dx11.ShaderResourceView>();
+        Dictionary<string, int> texIndex = new Dictionary<string, int>();
+
+        public List<Dx11.ShaderResourceView> GetAllTextures()
+        {
+            return textures;
+        }
+
+        public Dx11.ShaderResourceView GetTexture(string matName)
+        {
+            try
+            {
+                int index = texIndex[matName];
+                return textures[index];
+            }
+            catch
+            {
+                Console.WriteLine("TextureManager: GetTexture matNum:" + matName + " is not found.");
+                return null;
+            }
+        }
+
+
+        public void SetTexture(string matName, Dx11.ShaderResourceView tex)
+        {
+            textures.Add(tex);
+            texIndex.Add(matName, textures.Count - 1);
+        }
+    }
+
+    /// <summary>
+    /// 各頂点の情報
+    /// </summary>
+    struct VertexPositionTexture
+    {
+        public Vector3 Position;
+        public Vector2 TextureCoordinate;
+
+        public static readonly Dx11.InputElement[] VertexElements = new[]
+        {
+            new Dx11.InputElement
+            {
+                SemanticName = "SV_Position",
+                Format = Dxgi.Format.R32G32B32_Float
+            },
+            new Dx11.InputElement
+            {
+                SemanticName = "TEXCOORD",
+                Format = Dxgi.Format.R32G32_Float,
+                AlignedByteOffset = Dx11.InputElement.AppendAligned
+            }
+        };
+
+        public static int SizeInBytes
+        {
+            get { return System.Runtime.InteropServices.Marshal.SizeOf(typeof(VertexPositionTexture)); }
+        }
+    }
+}
---
+++ SlimDXTest/XConverter/XFileConverter.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SlimDXTest
+{
+    public class XFileConverter
+    {
+        public string FilePath { get; }
+        string fileName;
+        StreamReader sr;
+
+        public MeshSection meshSection;
+        public MaterialList matList;
+
+        // 1行1行解釈してクラスを作ってく 
+        private void Parser(string s)
+        {
+            Console.WriteLine("Parse:" + s);
+            if (Regex.IsMatch(s, @"^\s*Material "))
+            {
+                s = Regex.Replace(s, "Material ", "");  // マッチさせる前に邪魔なのは消す 
+                Match m = Regex.Match(s, "\\w+");
+                Console.WriteLine("Converter: Material.Add:" + m.Value);
+                matList.AddMaterial(sr, m.Value);
+            }
+            else if (Regex.IsMatch(s, @"^\s*Mesh "))
+            {
+                s = Regex.Replace(s, "Mesh ", "");
+                Match m = Regex.Match(s, "\\w+");
+                Console.WriteLine(m.Value);
+                meshSection = new MeshSection(sr, m.Value);
+            }
+        }
+
+        public XFileConverter(string filePath, string newPath)
+        {
+            //Xファイルの前変換
+            Console.WriteLine("XfileImporter Initialized.");
+            this.FilePath = MaterialFix(filePath, newPath);
+            fileName = System.IO.Path.GetFileName(this.FilePath);
+
+            Console.WriteLine("FilePath:" + FilePath);
+            Console.WriteLine("fileName:" + fileName);
+        }
+
+        public string Import()
+        {
+            sr = new StreamReader(FilePath);
+            matList = new MaterialList(sr);
+
+            while (!sr.EndOfStream)
+            {
+                Parser(sr.ReadLine());
+            }
+            return this.FilePath;
+        }
+
+        /// <summary>
+        /// マテリアルを直接書いている場合、参照呼び出しに変換する。
+        /// </summary>
+        /// <param name="fullPath">元のファイルパス</param>
+        /// <returns>変換後のファイルパス</returns>
+        private string MaterialFix(string filePath, string newPath)
+        {
+            Console.WriteLine("Convert:MaterialFix:)");
+
+            string name = System.IO.Path.GetFileName(filePath).Split('.')[0];
+
+            if (System.IO.File.Exists(newPath))
+            {
+                //変換したファイルがすでにあるので飛ばす
+                Console.WriteLine("ConvertedXFile is already Exists.");
+                return newPath;
+            }
+
+            StreamReader streamReader = new StreamReader(filePath);
+
+            //参照呼び出しか判定
+            List<string> xfile = new List<string>();
+            bool isHeaderBlock = false, isMeshBlock = false;
+            int headerPos = 1;
+            int matName = 0;
+            while (!streamReader.EndOfStream)
+            {
+                string line = streamReader.ReadLine();
+                xfile.Add(line);
+                if (Regex.IsMatch(line, @"^\s*Header\s*\{"))
+                {
+                    //ヘッダーブロック内
+                    isHeaderBlock = true;
+                    continue;
+                }
+
+                if (isHeaderBlock)
+                {
+                    //ヘッダーブロック終了判定
+                    if (Regex.IsMatch(line, @"\}"))
+                    {
+                        isHeaderBlock = false;
+                        headerPos = xfile.Count;
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (Regex.IsMatch(line, @"MeshMaterialList\s*\{"))
+                    {
+                        //マテリアルリストブロック内
+                        isMeshBlock = true;
+                        continue;
+                    }
+                    if (isMeshBlock)
+                    {
+                        if (Regex.IsMatch(line, @"\{.+\}"))
+                        {
+                            //参照呼び出しである
+                            Console.WriteLine("マテリアル参照呼び出し");
+                            return filePath;
+                        }
+                        else if (Regex.IsMatch(line, @"Material\s*\{"))
+                        {
+                            Console.WriteLine("----マテリアル直接呼び出し----");
+                            //直接呼び出しなので移動する
+                            List<string> material = new List<string>();
+                            material.Add("Material M" + matName + " {");
+                            xfile[xfile.Count - 1] = "  { M" + matName + " }";
+                            matName++;
+                            int block = 1;
+                            while (block > 0)
+                            {
+                                line = streamReader.ReadLine();
+                                Console.WriteLine("line:" + line);
+                                if(Regex.IsMatch(line, "\".+\"\\s*;"))
+                                {
+                                    //テクスチャファイルの移動
+                                    string texName = line.Split('"')[1];
+                                    string currentDir = System.IO.Path.GetDirectoryName(filePath) + "\\";
+                                    string newDir = System.IO.Path.GetDirectoryName(newPath) + "\\";
+                                    if (System.IO.File.Exists(currentDir + texName) && !System.IO.File.Exists(newDir + texName))
+                                    {
+                                        //ファイルが存在するので移動
+                                        System.IO.File.Copy(currentDir + texName, newDir + texName);
+                                        Console.WriteLine("CopyFile:" + currentDir + texName + " -> " + newDir + texName);
+                                    }
+                                }
+                                if (Regex.IsMatch(line, @"\}"))
+                                    block--;
+                                material.Add(CreateSpace(block) + line.Trim());
+                                if (Regex.IsMatch(line, @"\{"))
+                                    block++;
+                            }
+                            xfile.InsertRange(headerPos, material);
+                        }
+                        else if (Regex.IsMatch(line, @"\}"))
+                            isMeshBlock = false;
+                    }
+                }
+            }
+
+            //書き出し
+            System.IO.File.WriteAllLines(newPath, xfile.ToArray());
+            return newPath;
+        }
+
+        private string CreateSpace(int num)
+        {
+            string result = "";
+            for (int i = 0; i < num; i++)
+            {
+                result = result + " ";
+            }
+            return result;
+        }
+    }
+}
